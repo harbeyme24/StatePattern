13519ccf9d0fc715e0863513768afd1f5ba8c442
Harbey M. Edroso <143273418+harbeyme24@users.noreply.github.com>
2024-03-07 00:09:27 +0800

    Add files via upload

---
+++ AccountState.java
@@ -0,0 +1,7 @@
+public interface AccountState {
+    public void activate(Account account);
+    public void suspend(Account account);
+    public void deposit(Double depositAmount, Account account);
+    public void withdraw(Double withdrawAmount, Account account);
+    public void close(Account account);
+}
